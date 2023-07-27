f00c6a862b65a15c1d1e6d6011725e345e79ca80
Tharindu Dananjana <129717991+tharinduxd@users.noreply.github.com>
2023-07-27 19:36:45 +0530

    Add New 3 Social Media and Release - 2.0
    Facebook Download
    Instagram  Download
    Twitter  Download

--- background.js
+++ background.js
@@ -1,21 +1,48 @@
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.contextMenus.create({
-      id: "downloadYouTube",
-      title: "Download This YouTube Video",
-      contexts: ["page", "link"],
-      documentUrlPatterns: ["*://*.youtube.com/*"]
-    });
+  chrome.contextMenus.create({
+    id: "downloadYouTube",
+    title: "Download This YouTube Video",
+    contexts: ["page", "link"],
+    documentUrlPatterns: ["*://*.youtube.com/*"]
   });
-  
-  chrome.contextMenus.onClicked.addListener(function(info, tab) {
-    if (info.menuItemId === "downloadYouTube") {
-      var youtubeLink = info.linkUrl || tab.url;
-      var saveFromUrl = 'https://en.savefrom.net/1-youtube-video-downloader-510by/?url=' + encodeURIComponent(youtubeLink);
-      openUrl(saveFromUrl);
-    }
+
+  chrome.contextMenus.create({
+    id: "downloadFacebook",
+    title: "Download This Facebook Video",
+    contexts: ["page", "link"],
+    documentUrlPatterns: ["*://*.facebook.com/*"]
   });
-  
-  function openUrl(url) {
-    chrome.tabs.create({ url: url });
+
+  chrome.contextMenus.create({
+    id: "downloadInstagram",
+    title: "Download This Instagram Video",
+    contexts: ["page", "link"],
+    documentUrlPatterns: ["*://*.instagram.com/*"]
+  });
+
+  chrome.contextMenus.create({
+    id: "downloadTwitter",
+    title: "Download This Twitter Video",
+    contexts: ["page", "link"],
+    documentUrlPatterns: ["*://*.twitter.com/*"]
+  });
+});
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+  var url = "";
+  if (info.menuItemId === "downloadYouTube") {
+    url = 'https://en.savefrom.net/1-youtube-video-downloader-510by/?url=' + encodeURIComponent(info.linkUrl || tab.url);
+  } else if (info.menuItemId === "downloadFacebook") {
+    url = 'https://en.savefrom.net/9-how-to-download-facebook-video-217.html?url=' + encodeURIComponent(info.linkUrl || tab.url);
+  } else if (info.menuItemId === "downloadInstagram") {
+    url = 'https://en.savefrom.net/274/download-from-instagram?url=' + encodeURIComponent(info.linkUrl || tab.url);
+  } else if (info.menuItemId === "downloadTwitter") {
+    url = 'https://en.savefrom.net/233/download-from-twitter?url=' + encodeURIComponent(info.linkUrl || tab.url);
   }
-  
+
+  openUrl(url);
+});
+
+function openUrl(url) {
+  chrome.tabs.create({ url: url });
+}
--- popup.js
+++ popup.js
@@ -1,30 +1,40 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var videoButton = document.getElementById('videoButton');
-    var mp3Button = document.getElementById('mp3Button');
-  
-    videoButton.addEventListener('click', function() {
-      var youtubeLink = document.getElementById('youtubeLink').value;
-      var saveFromUrl = 'https://en.savefrom.net/1-youtube-video-downloader-510by/?url=' + encodeURIComponent(youtubeLink);
-      openUrl(saveFromUrl);
-    });
-  
-    mp3Button.addEventListener('click', function() {
-      var youtubeLink = document.getElementById('youtubeLink').value;
-      var x2downloadUrl = 'https://x2download.app/en98/download-youtube-to-mp3?url=' + encodeURIComponent(youtubeLink);
-      openUrl(x2downloadUrl);
-    });
-  
-    function openUrl(url) {
-      chrome.tabs.create({ url: url });
-    }
-  
-    // Automatically paste the copied link into the input field
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { action: 'getYouTubeLink' }, function(response) {
-        if (response && response.youtubeLink) {
-          document.getElementById('youtubeLink').value = response.youtubeLink;
-        }
-      });
+  var downloadYouTube = document.getElementById('downloadYouTube');
+  var downloadFacebook = document.getElementById('downloadFacebook');
+  var downloadInstagram = document.getElementById('downloadInstagram');
+  var downloadTwitter = document.getElementById('downloadTwitter');
+  var videoLink = document.getElementById('videoLink');
+
+  downloadYouTube.addEventListener('click', function() {
+    var saveFromUrl = 'https://en.savefrom.net/1-youtube-video-downloader-510by/?url=' + encodeURIComponent(videoLink.value);
+    openUrl(saveFromUrl);
+  });
+
+  downloadFacebook.addEventListener('click', function() {
+    var saveFromUrl = 'https://en.savefrom.net/9-how-to-download-facebook-video-217.html?url=' + encodeURIComponent(videoLink.value);
+    openUrl(saveFromUrl);
+  });
+
+  downloadInstagram.addEventListener('click', function() {
+    var saveFromUrl = 'https://en.savefrom.net/274/download-from-instagram?url=' + encodeURIComponent(videoLink.value);
+    openUrl(saveFromUrl);
+  });
+
+  downloadTwitter.addEventListener('click', function() {
+    var saveFromUrl = 'https://en.savefrom.net/233/download-from-twitter?url=' + encodeURIComponent(videoLink.value);
+    openUrl(saveFromUrl);
+  });
+
+  function openUrl(url) {
+    chrome.tabs.create({ url: url });
+  }
+
+  // Automatically paste the copied link into the input field
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { action: 'getYouTubeLink' }, function(response) {
+      if (response && response.youtubeLink) {
+        videoLink.value = response.youtubeLink;
+      }
     });
   });
-  
+});
